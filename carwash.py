879a6f273352908920d81fd209ad0c58d503d663
Bill DeRose <bill.derose11@gmail.com>
2015-07-21 15:26:30 -0700

    Format carwash.py

--- carwash.py
+++ carwash.py
@@ -7,24 +7,24 @@ s3_path = "s3://carwashr/cluster"
 image_path = "s3://carwashr/images"
 label_path = "s3://carwashr/labels.csv"
 
-ec2 = gl.deploy.ec2_cluster.create(name='carwashr-4',
-                                   s3_path=s3_path,
-                                   ec2_config=gl.deploy.Ec2Config(),
-                                   num_hosts=1)
+ec2 = gl.deploy.ec2_cluster.create(name='carwashr-4', s3_path=s3_path, ec2_config=gl.deploy.Ec2Config())
+
 
 def train_carwash_network():
-    sf = gl.image_analysis.load_images(image_path, "auto", with_path=True,recursive=False, random_order=False)
+    sf = gl.image_analysis.load_images(image_path, "auto", with_path=True, recursive=False, random_order=False)
     sf["id"] = sf["path"].apply(lambda path: os.path.basename(path)[:-4]).astype(int)
     labels = gl.SFrame(label_path)
     sf = sf.join(labels, on="id", how="right")  # only include images with labels
-    sf["label"] = sf.apply(lambda row: int(Counter(row["labels"]).most_common()[0][0]))  # use mode label as authoritative label
-    print sf["label"]
+    sf["label"] = sf.apply(
+        lambda row: int(Counter(row["labels"]).most_common()[0][0]))  # use mode label as authoritative label
     train, test = sf.random_split(0.8)
-    feature_extractor = gl.feature_engineering.create(sf, DeepFeatureExtractor(feature="image",
-                                                                               output_column_name="deep_features"))
+    feature_extractor = gl.feature_engineering.create(sf,
+                                                      DeepFeatureExtractor(feature="image",
+                                                                           output_column_name="deep_features"))
     deep_sf = feature_extractor.transform(sf)
     c = gl.classifier.create(deep_sf, target='label', features=["deep_features"])
     return c.evaluate(test)
+
 
 ob_ec2 = gl.deploy.job.create(train_carwash_network, environment=ec2)
 print ob_ec2.get_results()

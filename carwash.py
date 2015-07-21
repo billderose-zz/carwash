cb526ea1473fda904d09c522a07224e077d44795
Bill DeRose <bill.derose11@gmail.com>
2015-07-21 14:46:26 -0700

    Deploy graphlab classifier to AWS

--- carwash.py
+++ carwash.py
@@ -1,54 +1,31 @@
 import graphlab as gl
-from statistics import mode
+from graphlab.toolkits.feature_engineering import DeepFeatureExtractor
+from collections import Counter
 import os
 
-loadModel = False
-image_path = "./images"
+s3_path = "s3://carwashr/cluster"
+image_path = "s3://carwashr/images"
+label_path = "s3://carwashr/labels.csv"
 
+ec2 = gl.deploy.ec2_cluster.create(name='carwashr-4',
+                                   s3_path=s3_path,
+                                   ec2_config=gl.deploy.Ec2Config(),
+                                   num_hosts=1)
 
-sf = gl.image_analysis.load_images(image_path, "auto", with_path=True, recursive=False, random_order=False)
-sf["image"] = gl.image_analysis.resize(sf["image"], 800, 800, 3)
-sf["id"] = sf["path"].apply(lambda path: os.path.basename(path)[:-4]).astype(int)
-print sf["id"]
-# sf["id"] = gl.SArray(range(len(sf["image"])))
+def train_carwash_network():
+    sf = gl.image_analysis.load_images(image_path, "auto", with_path=True,recursive=False, random_order=False)
+    sf["id"] = sf["path"].apply(lambda path: os.path.basename(path)[:-4]).astype(int)
+    labels = gl.SFrame(label_path)
+    sf = sf.join(labels, on="id", how="right")  # only include images with labels
+    sf["label"] = sf.apply(lambda row: int(Counter(row["labels"]).most_common()[0][0]))  # use mode label as authoritative label
+    print sf["label"]
+    train, test = sf.random_split(0.8)
+    feature_extractor = gl.feature_engineering.create(sf, DeepFeatureExtractor(feature="image",
+                                                                               output_column_name="deep_features"))
+    deep_sf = feature_extractor.transform(sf)
+    c = gl.classifier.create(deep_sf, target='label', features=["deep_features"])
+    return c.evaluate(test)
 
-labels = gl.SFrame("./labels.csv")
-print labels
-print "Joining..."
-sf = sf.join(labels, on="id", how="right")  # only include images with labels
-sf["label"] = sf.apply(lambda row: int(mode(row["labels"]))) # use mode label as authoritative label
-print "Labeled"
-print sf["label"]
-
-train, test = sf.random_split(0.8)
-if not loadModel:
-    nnet = gl.deeplearning.create(train, target='label', features=["image"])
-    m = gl.neuralnet_classifier.create(train, target='label', features=["image"], network=nnet,
-                                       metric=['accuracy', 'recall@2', 'precision', "fdr", "fp"], validation_set=test, max_iterations=3)
-    m.save("./gl.carwash.model")
-else:
-    m = gl.load_model("./gl.carwash.model")
-
-print m.evaluate(test)
-print m.predict(test)
-print gl.evaluation.confusion_matrix(test["label"], m.predict)
-
-
-def similarity(net, im1, im2):
-    features = net.extract_features(gl.SFrame({'image': [im1, im2]}))
-    return gl.distances.cosine(features[0], features[1])
-
-def print_similarity_matrix(sframe):
-    s = ""
-    for i, x in enumerate(sframe["image"]):
-        if i == 0:
-            s += "%d.jpg" % i
-        else:
-            s += "\t%d.jpg" % i
-    print s
-    for i, x in enumerate(sframe["image"]):
-        s = "%d.jpg" % i
-        for y in sf["image"]:
-            s += "\t%.2f" % similarity(m, x, y)
-        print s
-        s = ""
+ob_ec2 = gl.deploy.job.create(train_carwash_network, environment=ec2)
+print ob_ec2.get_results()
+ec2.stop()
